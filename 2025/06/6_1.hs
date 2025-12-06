608a89b508abcd7b0ad36cd48b1398a4635bda2b
UPEV1sion <e.schaeffer82@gmail.com>
2025-12-06 10:29:49 +0100

    6/1

---
+++ 2025/06/6_1.hs
@@ -0,0 +1,16 @@
+import Data.Maybe
+import Data.List ( transpose )
+
+evalCol :: [String] -> Int
+evalCol col = 
+    let (ops, [op]) = splitAt 4 col
+        nums = read <$> ops
+    in case op of
+        "+" -> sum nums
+        "*" -> product nums
+
+main :: IO()
+main = do
+    input <- lines <$> readFile "6.in"
+    let cols = transpose $ words <$> input
+    print $ sum $ evalCol <$> cols
